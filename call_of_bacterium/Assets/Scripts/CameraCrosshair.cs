57f39e1b442464d50ac47f19f219e200d4eebf67
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2021-10-28 22:32:40 -0500

    Crosshair raycast and shoot

--- call_of_bacterium/Assets/Scripts/CameraCrosshair.cs
+++ call_of_bacterium/Assets/Scripts/CameraCrosshair.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraCrosshair : MonoBehaviour
 {
-    public const float maxDistance = 10;
-    private GameObject gazedAtObject = null;
+    [Header("Raycast")]
+    [SerializeField] private float maxDistance = 10;
 
+    [Header("Crosshair")]
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private Sprite crosshair1Sprite;
+    [SerializeField] private Sprite crosshair2Sprite;
+    
+    [Header("Shoot options")]
+    [SerializeField] private Transform pillSpawn;
+    [SerializeField] private Transform pill;
+
+    private GameObject gazedAtObject = null;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +29,36 @@ public class CameraCrosshair : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
+        Image crosshairImg = crosshair.GetComponent<Image>();
+
         if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
         {
-            // GameObject detected in front of the camera.
-            if (gazedAtObject != hit.transform.gameObject)
+            // Enemy detected in front of the camera.
+            if (gazedAtObject != hit.transform.gameObject && hit.transform.tag == "Enemy")
             {
-                // New GameObject.
-                
+                // Change crosshair color and sprite
+                crosshairImg.sprite = crosshair2Sprite;
+                crosshairImg.color = Color.red;
+
+                // Shoot
+                ShootPill(pillSpawn.position, hit.transform.position);
+                //Transform pillTransform = Instantiate(pill, pillSpawn.position, Quaternion.identity);
+
             }
         }
+        else
+        {
+            // No GameObject detected in front of the camera.
+            crosshairImg.sprite = crosshair1Sprite;
+            crosshairImg.color = Color.black;
+            gazedAtObject = null;
+        }
+    }
+
+    private void ShootPill(Vector3 spawnPos, Vector3 endPos)
+    {
+        Transform pillTransform = Instantiate(pill, spawnPos, Quaternion.identity);
+        Vector3 shootDir = (endPos - spawnPos).normalized;
+        pillTransform.GetComponent<Pill>().Setup(shootDir);
     }
 }
---
+++ call_of_bacterium/Assets/Scripts/Pill.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pill : MonoBehaviour
+{
+    private Vector3 shootDir;
+    public void Setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+        Destroy(gameObject, 5f);
+    }
+
+    private void Update() {
+        float speed = 100f;
+        transform.position += shootDir * speed * Time.deltaTime;
+    }
+}
